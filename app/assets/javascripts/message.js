d1a2c05bf8bb3c54ee7d14ddf778e6ad25a45495
kumapaaan <kumapaaan@users.noreply.github.com>
2020-06-12 17:13:56 +0900

    ajax修正１

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,7 +42,7 @@ $(function(){
           };
         }
 
-  $('form').on('submit', function(e){
+  $('#form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
